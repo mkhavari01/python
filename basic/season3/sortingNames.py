a7807cffc845667865132172f5439500cf8a9ba8
Mahdi Khavari <mkhavari813@gmail.com>
2022-05-11 11:43:18 +0430

    season3 ex3

---
+++ basic/season3/sortingNames.py
@@ -0,0 +1,13 @@
+# test = ["mahdi","hassan","ali"]
+# test.sort()
+# print(test)
+
+namesList = []
+
+for i in range(0,10):
+  name = input("Enter the name: ");
+  namesList.append(name);
+
+namesList.sort();
+
+print(namesList)
